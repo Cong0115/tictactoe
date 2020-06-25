4d43457809c58fdceb61b07eb4b3c6cf398802b3
Cong0115 <64848735+Cong0115@users.noreply.github.com>
2020-06-25 08:57:36 -0400

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,125 +1,77 @@
 import numpy as np
 import random
 import time
+from tkinter import *
 
 
-def iif(cond, t, f):
-    if cond:
-        return t
-    else:
-        return f
-
-
-def printboard(board):
-    marker = ["", "X", "", "", "O"]
-    boardout = np.array([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
-    for row in range(3):
-        for col in range(3):
-            if board[row, col] != 0:
-                boardout[row, col] = marker[board[row, col]]
-    print('------------------')
-    print(" {} | {} | {}".format(
-        boardout[0, 0], boardout[0, 1], boardout[0, 2]))
-    print("-----------")
-    print(" {} | {} | {}".format(
-        boardout[1, 0], boardout[1, 1], boardout[1, 2]))
-    print("-----------")
-    print(" {} | {} | {}".format(
-        boardout[2, 0], boardout[2, 1], boardout[2, 2]))
-    print('------------------')
-
-
-def getwinmove(board):
+def getwinmove(board, playernum=4):
+    totalcheck = playernum*2      # 2 if player 1, 8 if player 4
     winmove = []
     # check rows
     for row in range(3):
-        if np.sum(board[row, :]) == 8:
+        if sum(board[row, :]) == totalcheck:
             for col in range(3):
                 if board[row, col] == 0:
                     winmove.append((row, col))
-                    # break
     # check columns
     for col in range(3):
-        if np.sum(board[:, col]) == 8:
+        if sum(board[:, col]) == totalcheck:
             for row in range(3):
                 if board[row, col] == 0:
                     winmove.append((row, col))
-                    # break
     # check top-left to bottom-right
-    if board[0, 0]+board[1, 1]+board[2, 2] == 8:
+    if board[0, 0]+board[1, 1]+board[2, 2] == totalcheck:
         for i in range(3):
             if board[i, i] == 0:
                 winmove.append((i, i))
     # check top-right to bottom-left
-    if board[0, 2]+board[1, 1]+board[2, 0] == 8:
+    if board[0, 2]+board[1, 1]+board[2, 0] == totalcheck:
         for i in range(3):
             if board[i, 2-i] == 0:
                 winmove.append((i, 2-i))
     return winmove
 
 
-def getblockmove(board):
+def getblockmove(board, playernum=4):
+    totalcheck = (5-playernum)*2      # 2 for player 4, 8 for player 1
     blockmove = []
     # check rows
     for row in range(3):
-        if np.sum(board[row, :]) == 2:
+        if sum(board[row, :]) == totalcheck:
             for col in range(3):
                 if board[row, col] == 0:
                     blockmove.append((row, col))
                     # break
     # check columns
     for col in range(3):
-        if np.sum(board[:, col]) == 2:
+        if sum(board[:, col]) == totalcheck:
             for row in range(3):
                 if board[row, col] == 0:
                     blockmove.append((row, col))
                     # break
     # check top-left to bottom-right
-    if board[0, 0]+board[1, 1]+board[2, 2] == 2:
+    if board[0, 0]+board[1, 1]+board[2, 2] == totalcheck:
         for i in range(3):
             if board[i, i] == 0:
                 blockmove.append((i, i))
     # check top-right to bottom-left
-    if board[0, 2]+board[1, 1]+board[2, 0] == 2:
+    if board[0, 2]+board[1, 1]+board[2, 0] == totalcheck:
         for i in range(3):
             if board[i, 2-i] == 0:
                 blockmove.append((i, 2-i))
     return blockmove
 
 
-def getforkmove(board):
+def getforkmove(board, playernum=4):
     forkmove = []
     for row in range(3):
         for col in range(3):
             if board[row, col] == 0:
                 tempboard = np.copy(board)
-                tempboard[row, col] = 4
-                if len(getwinmove(tempboard)) > 1:
+                tempboard[row, col] = playernum
+                if len(getwinmove(tempboard, playernum)) > 1:
                     forkmove.append((row, col))
     return forkmove
-
-
-def getplayermove(board):
-    while True:
-        pm = int(input('Your move (1-9)? '))
-        if pm in range(1, 10):
-            row = (pm-1)//3
-            col = ((pm-1) % 3)
-            if board[row, col] == 0:
-                return (row, col)
-
-
-def getadjacent(board, row, col):
-    adj = 0
-    if row > 0:
-        adj += iif(board[row-1, col] == 1, 1, 0)
-    if row < 2:
-        adj += iif(board[row+1, col] == 1, 1, 0)
-    if col > 0:
-        adj += iif(board[row, col-1] == 1, 1, 0)
-    if col < 2:
-        adj += iif(board[row, col+1] == 1, 1, 0)
-    return adj
 
 
 def getaimove(board):
@@ -136,87 +88,183 @@ def getaimove(board):
         return (1, 1)  # go in center
     # 4. are we going second?
     if np.count_nonzero(board) == 1:
-        if board[1, 1] == 1:  # did player go in center?
+        if board[1, 1] == 1:        # did player go in center?
             # go in a random corner
             return (random.randint(0, 1)*2, random.randint(0, 1)*2)
         else:
-            return (1, 1)  # otherwise go in center
+            # otherwise go in center
+            return (1, 1)           
     # 5. do we have a fork move (i.e. a move that results in 2 winning moves for us)?
     move = getforkmove(board)
     if len(move) > 0:
         return random.choice(move)
-    # 6. otherwise: go next to a player space, with a winning move if possible
+    # 6. any available move that leaves us a winning move, and blocking doesn't leave the player more than 1 winning move
     move = []
-    # first check for moves that result in a winning move
-    # we know that no moves will give us more than 1 winning move, because we would've caught it as a fork move
     for row in range(3):
         for col in range(3):
-            if board[row, col] == 0 and getadjacent(board, row, col) > 0:
+            if board[row, col] == 0:
                 tempboard = np.copy(board)
                 tempboard[row, col] = 4
-                if len(getwinmove(tempboard)) > 0:
+                # does it leave us a winning move? (there can only be 1, otherwise we would've seen it as a fork move)
+                aiwin = getwinmove(tempboard)
+                if len(aiwin) > 0:
+                    tempboard[aiwin[0][0], aiwin[0][1]] = 1
+                    # make sure the player has less than 2 winning moves after blocking us
+                    if len(getwinmove(tempboard, 1)) < 2:
+                        move.append((row, col))
+    if len(move) > 0:
+        return random.choice(move)
+    # 7. any available move that doesn't leave a fork move for the player
+    # apparently we never even get here?
+    for row in range(3):
+        for col in range(3):
+            if board[row, col] == 0:
+                tempboard = np.copy(board)
+                tempboard[row, col] = 4
+                if len(getforkmove(tempboard, 1)) == 0:
                     move.append((row, col))
     if len(move) > 0:
         return random.choice(move)
-    # settle for move that doesn't result in a winning move
+    # otherwise: any random available move
     for row in range(3):
         for col in range(3):
-            if board[row, col] == 0 and getadjacent(board, row, col) > 0:
+            if board[row, col] == 0:
                 move.append((row, col))
     if len(move) > 0:
         return random.choice(move)
-    # we shouldn't get here
-    print('We shouldn\'t get here...')
 
+
+def playagain(event):
+    global board
+    global gameover
+    cv.unbind('<Button-1>')
+    cv.create_rectangle(0, 0, 500, 500, outline='black', fill='black')
+    # draw the board lines
+    cv.create_line(25, 175, 475, 175, width=10, fill='white')
+    cv.create_line(25, 325, 475, 325, width=10, fill='white')
+    cv.create_line(175, 25, 175, 475, width=10, fill='white')
+    cv.create_line(325, 25, 325, 475, width=10, fill='white')
+    #
+    label2['text'] = ''
+    gameover = False
+    board = np.zeros((3, 3), dtype=np.int8)
+    if (playerwin+aiwin+draw) % 2 == 1:         # computer goes first on odd games
+        # get computer move
+        move = getaimove(board)
+        # draw O for computer:
+        cv.create_oval(move[0]*150+50, move[1]*150+50, move[0]
+                       * 150+150, move[1]*150+150, width=5, outline='blue')
+        board[move] = 4
+    cv.bind('<Button-1>', gameloop)
+
+
+def gameloop(event):
+    global board
+    global gameover
+    global playerwin, aiwin, draw
+    cv.unbind('<Button-1>')
+    label2['text']=''
+    if event.x < 175:
+        row = 0
+    elif event.x < 325:
+        row = 1
+    else:
+        row = 2
+    if event.y < 175:
+        col = 0
+    elif event.y < 325:
+        col = 1
+    else:
+        col = 2
+    if board[row, col] == 0:        # is move valid?
+        # draw X for player:
+        cv.create_line(row*150+50, col*150+50, row*150+150,
+                       col*150+150, width=5, fill='red')
+        cv.create_line(row*150+150, col*150+50, row*150 +
+                       50, col*150+150, width=5, fill='red')
+        board[row, col] = 1
+        # see if player won:
+        if sum(board[0, :]) == 3 or \
+                sum(board[1, :]) == 3 or \
+                sum(board[2, :]) == 3 or \
+                sum(board[:, 0]) == 3 or \
+                sum(board[:, 1]) == 3 or \
+                sum(board[:, 2]) == 3 or \
+                sum(board[i,i] for i in range(3)) == 3 or \
+                sum(board[i,2-i] for i in range(3)) == 3:
+            playerwin += 1
+            label1['text'] = 'Win/Lose/Draw: {}/{}/{}'.format(
+                playerwin, aiwin, draw)
+            label2['text'] = 'You won!  Click the board to play again'
+            gameover = True
+        # or if game ended in a draw:
+        elif np.count_nonzero(board) == 9:
+            draw += 1
+            label1['text'] = 'Win/Lose/Draw: {}/{}/{}'.format(
+                playerwin, aiwin, draw)
+            label2['text'] = 'The game ends in a draw.  Click the board to play again'
+            gameover = True
+        else:
+            # get computer move
+            ## this part doesn't work as expected:
+            label2['text'] = 'Computer thinking...'
+            time.sleep(0.5)
+            label2['text'] = ''
+            ##
+            move = getaimove(board)
+            # draw O for computer:
+            cv.create_oval(move[0]*150+50, move[1]*150+50, move[0]
+                           * 150+150, move[1]*150+150, width=5, outline='blue')
+            board[move] = 4
+            # see if AI won:
+            if sum(board[0, :]) == 12 or \
+                    sum(board[1, :]) == 12 or \
+                    sum(board[2, :]) == 12 or \
+                    sum(board[:, 0]) == 12 or \
+                    sum(board[:, 1]) == 12 or \
+                    sum(board[:, 2]) == 12 or \
+                    sum(board[i,i] for i in range(3)) == 12 or \
+                    sum(board[i,2-i] for i in range(3)) == 12:
+                aiwin += 1
+                label1['text'] = 'Win/Lose/Draw: {}/{}/{}'.format(
+                    playerwin, aiwin, draw)
+                label2['text'] = 'The computer won.  Click the board to play again'
+                gameover = True
+            elif np.count_nonzero(board) == 9:
+                draw += 1
+                label1['text'] = 'Win/Lose/Draw: {}/{}/{}'.format(
+                    playerwin, aiwin, draw)
+                label2['text'] = 'The game ends in a draw.  Click the board to play again'
+                gameover = True
+    if gameover:
+        cv.bind('<Button-1>', playagain)
+    else:
+        cv.bind('<Button-1>', gameloop)
+
+
+root = Tk()
+
+cvwidth = 500
+cvheight = 500
+cv = Canvas(root, bg='black', width=cvwidth, height=cvheight)
+cv.pack(side=TOP)
+# draw the board lines
+cv.create_line(25, 175, 475, 175, width=10, fill='white')
+cv.create_line(25, 325, 475, 325, width=10, fill='white')
+cv.create_line(175, 25, 175, 475, width=10, fill='white')
+cv.create_line(325, 25, 325, 475, width=10, fill='white')
+
+frame = Frame(root)
+frame.pack(side=BOTTOM, fill=BOTH)
+label1 = Label(frame, text='Win/Lose/Draw: 0/0/0')
+label2 = Label(frame, text='Click the board to play!')
+label1.pack()
+label2.pack()
 
 playerwin, aiwin, draw = 0, 0, 0
-playagain = True
 
-while playagain:
-    gameover = False
-    # player=1, ai=4. first player is random
-    curplayer = random.choice([1, 4])
-    board = np.zeros((3, 3), dtype=np.int8)
-    printboard(board)
-    # game loop
-    while not gameover:
-        if curplayer == 1:
-            move = getplayermove(board)
-        else:
-            move = getaimove(board)
-            time.sleep(0.5)
-            print('Computer moves to: {}'.format(move[0]*3+move[1]+1))
-        board[move] = curplayer
-        printboard(board)
-        # see if player won:
-        if np.sum(board[0, :]) == 3 or \
-                np.sum(board[1, :]) == 3 or \
-                np.sum(board[2, :]) == 3 or \
-                np.sum(board[:, 0]) == 3 or \
-                np.sum(board[:, 1]) == 3 or \
-                np.sum(board[:, 2]) == 3 or \
-                board[0, 0] + board[1, 1]+board[2, 2] == 3 or \
-                board[0, 2] + board[1, 1]+board[2, 0] == 3:
-            print('You won!')
-            playerwin += 1
-            gameover = True
-        # see if AI won:
-        if np.sum(board[0, :]) == 12 or \
-                np.sum(board[1, :]) == 12 or \
-                np.sum(board[2, :]) == 12 or \
-                np.sum(board[:, 0]) == 12 or \
-                np.sum(board[:, 1]) == 12 or \
-                np.sum(board[:, 2]) == 12 or \
-                board[0, 0] + board[1, 1]+board[2, 2] == 12 or \
-                board[0, 2] + board[1, 1]+board[2, 0] == 12:
-            print('The computer won :/')
-            aiwin += 1
-            gameover = True
-        # see if we ended in a draw:
-        if np.count_nonzero(board) == 9:
-            print('The game ends in a draw......')
-            draw += 1
-            gameover = True
-        curplayer = 5-curplayer
-    print('Win: {}   Lose: {}   Draw: {}'.format(playerwin, aiwin, draw))
-    time.sleep(1.0)
+gameover = False
+board = np.zeros((3, 3), dtype=np.int8)
+
+cv.bind('<Button-1>', gameloop)
+root.mainloop()
